79902cb207b5fca50c90fa7b783448f41152a3b0
JingfeiPeng <pengjefferbms@gmail.com>
2020-01-16 20:56:29 -0500

    done 36+36

--- 37Soduku/python.py
+++ 37Soduku/python.py
@@ -1,17 +1,32 @@
-class Solution(object):
+class SudokuSolver(object):
     def solveSudoku(self, board):
-        solver(board)
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        
+        self.solver(board)
     
-    def solver(board):
+    def solver(self, board):
         for i in range(9):
             for j in range(9):
                 if (board[i][j] == '.'):
                     count = '1'
-                    while (count <= '9'):
-                        if (isValid(i,j,board, count)):
-                            print("ok")
+                    while (int(count) <= 9):
+                        if self.isValid(i,j,board, count):
+                            board[i][j] = count
+                            if self.solver(board):
+                                return True
+                            else:
+                                board[i][j] = '.'
+                        count = str(int(count) + 1)
+                    
+                    return False
+                
+        return True
+
     
-    def isValid(row, col, board, c):
+    def isValid(self, row, col, board, c):
         for i in range(9):
             if board[row][i] == c:
                 return False
@@ -20,8 +35,8 @@ class Solution(object):
             if board[i][col] == c:
                 return False
             
-        int startRow = int(row/3)*3
-        int startCol = int(col/3)*3
+        startRow = int(row/3)*3
+        startCol = int(col/3)*3
         
         for i in range(3):
             for j in range(3):
@@ -29,4 +44,3 @@ class Solution(object):
                     return False
         
         return True
-            
